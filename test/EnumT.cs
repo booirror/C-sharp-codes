99fbbe84737e094764d7f6ce68f8a77f147e7308
booirror <booirror@gmail.com>
2015-09-24 22:30:48 +0800

    enum

---
+++ test/EnumT.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+enum LightColor
+{
+    Red,
+    Yellow,
+    Green
+}
+
+class TrafficLight
+{
+    public static void WhatInfo(LightColor color)
+    {
+        switch (color)
+        {
+            case LightColor.Red:
+                Console.WriteLine(LightColor.Red.ToString());
+                Console.WriteLine("Stop");
+                break;
+            case LightColor.Yellow:
+                Console.WriteLine("warning");
+                break;
+            case LightColor.Green:
+                Console.WriteLine("Go");
+                break;
+            default:
+                break;
+        }
+    }
+}
+
