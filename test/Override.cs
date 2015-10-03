d35671fc9d48a2f3bfb14f515c016ab5eb122488
booirror <booirror@gmail.com>
2015-10-03 22:23:35 +0800

    override

---
+++ test/Override.cs
@@ -0,0 +1,47 @@
+﻿public class Override
+{
+    public class BaseClass
+    {
+        public void DisplayName()
+        {
+            System.Console.WriteLine("BaseClass");
+        }
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        public virtual void DisplayName()
+        {
+            System.Console.WriteLine("DerivedClass");
+        }
+    }
+
+    public class SubDerivedClass : DerivedClass
+    {
+        public override void DisplayName()
+        {
+            System.Console.WriteLine("SubDerivedClass");
+        }
+    }
+
+    public class SuperSubDerivedClass : SubDerivedClass
+    {
+        public new void DisplayName()
+        {
+            System.Console.WriteLine("SuperSubDerivedClass");
+        }
+    }
+
+    public static void Test()
+    {
+        SuperSubDerivedClass super = new SuperSubDerivedClass();
+        SubDerivedClass sub = super;
+        DerivedClass derived = super;
+        BaseClass basec = super;
+
+        super.DisplayName();
+        sub.DisplayName();
+        derived.DisplayName();
+        basec.DisplayName();
+    }
+}
