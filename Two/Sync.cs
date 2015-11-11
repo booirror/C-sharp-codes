c5c17f6a5f4dfd14f5dc3fcb63946d317ab988cb
booirror <booirror@gmail.com>
2015-11-11 23:36:17 +0800

    thread

---
+++ Two/Sync.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadTs
+{
+    class Sync
+    {
+        public static void Test()
+        {
+            ArrayList myAL = new ArrayList();
+            myAL.Add("The");
+            myAL.Add("quick");
+            myAL.Add("fox");
+
+            ArrayList mys = ArrayList.Synchronized(myAL);
+
+            Console.WriteLine(mys.IsSynchronized);
+        }
+    }
+}
