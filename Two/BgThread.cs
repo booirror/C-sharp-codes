f46256a5ec456b7f930c169008f1f8f8ca42a1a6
booirror <booirror@gmail.com>
2015-11-18 15:41:23 +0800

    bg

---
+++ Two/BgThread.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadT
+{
+    class BgThread
+    {
+    public static void Test()
+    {
+        Thread t = new Thread(ThreadProc);
+        //t.IsBackground = true;  // 加这一句
+        t.Start();
+ 
+        //t.Join();
+    }
+    static void ThreadProc(object state)
+    {
+        for(int i=0; i<10000; i++)
+        {
+            Console.WriteLine(".");
+            Thread.Sleep(300);
+        }
+    }
+
+    }
+}
